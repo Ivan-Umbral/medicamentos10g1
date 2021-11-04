6b59a337daac5e763d9a1d606dd051cd1c8f58d2
Oscar Reyes <reyesmartinezoscar3g15@gmail.com>
2021-11-04 15:45:45 -0600

    Cambiando a puerto dinámico para deploy

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { APP_PORT } from './config/constants.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>(APP_PORT);
   const config = new DocumentBuilder()
     .setTitle('Medicamentos API')
     .setDescription('Sistema de medicamentos 10G1')
@@ -16,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port);
 }
 bootstrap();
